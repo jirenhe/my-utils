ba46a4d508e60eadb52712820335b2654987cf04
jirenhe <jirenhe@wshifu.com>
2019-05-17 16:33:35 +0800

    1

--- auto_delete_kindle_copy_content.py
+++ auto_delete_kindle_copy_content.py
@@ -1,5 +1,6 @@
 import re
 import time
+import traceback
 
 import win32clipboard as wincld
 import win32con
@@ -23,8 +24,12 @@ def set_text(info):
     wincld.CloseClipboard()
 
 
-pattern = re.compile(r"\r\n\r\n杨保华.*Kindle 版本.")
+pattern = re.compile(r"\r\n\r\n.*Kindle 版本.")
 pattern2 = re.compile(r"([^\x00-\xff])\s+([^\x00-\xff])")
+pattern3 = re.compile(r"([/:.\\<[_-])\s+")
+pattern4 = re.compile(r"(?<!\s)(\[[a-zA-Z]+?.*?\])")
+pattern5 = re.compile(r"([A-Za-z])(<.*?>)")
+pattern6 = re.compile(r"([a-zA-Z])(-+)")
 
 while True:
     try:
@@ -32,7 +37,14 @@ while True:
         if s:
             s = re.sub(pattern, "", s)
             s = re.sub(pattern2, lambda x: x.group(1) + x.group(2), s)
+            s = re.sub(pattern3, lambda x: x.group(1), s)
+            s = re.sub(pattern4, lambda x: " " + x.group(1), s)
+            s = re.sub(pattern5, lambda x: x.group(1) + " " + x.group(2), s)
+            s = re.sub(pattern6, lambda x: x.group(1) + " " + x.group(2), s)
             set_text(s)
         print(s)
+    except Exception as e:
+        traceback.print_exc()
+        pass
     finally:
         time.sleep(1)
